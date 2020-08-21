65b203dce692609706a4798be5aeffba091c2e99
romeodelosangeles <romeodelosangeles@gmail.com>
2020-08-20 18:35:25 -0700

    Faker Gem example

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Post.create(title: Faker::Movies::StarWars.call_number, author: Faker::Movies::StarWars.character, description: Faker::Movies::StarWars.quote)  
+end
